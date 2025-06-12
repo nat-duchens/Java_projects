03b34d25a49839ceea719d1b35e69dc90ff646f8
natalie <natalie.nd92@gmail.com>
2025-06-12 12:04:56 -0400

    feat(docs): add new files of Comparison Operators excersises

---
+++ Java_basic/src/C20120625/ComparisonOperators.java
@@ -0,0 +1,15 @@
+package C20120625;
+
+public class ComparisonOperators {
+    public static void main(String[] args) {
+        //Comparison operators have results as true or false
+        /*
+        * == -> igual que
+        * > -> mayor que
+        * < -> menor que
+        * >= -> mayor o igual que
+        * <= -> menor o igual que
+        * != -> distinto de
+        * */
+    }
+}
