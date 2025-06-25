f62c28d0c7404d62d4922079884dc7e6199eb1d8
natalie <natalie.nd92@gmail.com>
2025-06-25 13:04:33 -0400

    feat(docs): upload Getter and Setter exercises

---
+++ Java_basic/src/C20250625_Getter_Setter/Main.java
@@ -0,0 +1,26 @@
+package C20250625_Getter_Setter;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Harry", 27);
+        //Show values using toString();
+        System.out.println(student1);
+
+        //Result without toString()
+        //C20250625_Getter_Setter.Student@8efb846
+
+        //Result with toString()
+        //Student{name: 'Harry', age 27}
+
+        //Show values using getter
+        System.out.println(" ");
+        System.out.println("Name: " + student1.getName());
+        System.out.println("Age: " + student1.getAge());
+
+        //toString() is like "translating" an object into readable text.
+        //Without main and sout, you can't print
+
+    }
+
+}
---
+++ Java_basic/src/C20250625_Getter_Setter/Student.java
@@ -0,0 +1,41 @@
+package C20250625_Getter_Setter;
+
+public class Student {
+
+    //Private attributes: cannot be accessed directly from outside the class
+    private String name;
+    private int age;
+
+    //Constructor: It is called automatically when creating a new object
+    public Student(String name, int age) {
+        //this -> refers to the attribute of the class
+        this.name = name; //Assign the parameter to the 'name' attribute
+        this.age = age;
+    }
+
+    //Getter -> allows you to read and obtain the value in a controlled manner
+    public String getName() {
+        return name;
+    }
+    //Setter -> allows you to read and change the value if needed
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString(){
+        return "Student{" +
+                "name: '" + name + '\'' +
+                ", age " + age +
+                '}';
+    }
+
+}
